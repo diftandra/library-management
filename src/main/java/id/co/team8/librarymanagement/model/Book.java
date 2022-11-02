4ff8e351fd0b705c20fa65da8503f3f7b51ed7f5
diftandra <45681770+diftandra@users.noreply.github.com>
2022-11-02 23:15:59 +0700

    Add files via upload

---
+++ src/main/java/id/co/team8/librarymanagement/model/Book.java
@@ -0,0 +1,36 @@
+package id.co.team8.librarymanagement.model;
+
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="book")
+@Data
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="book_id")
+	private Integer bookId;
+	
+	@Column(name="book_name")
+	private String bookName;
+	
+	@Column(name="book_author")
+	private String bookAuthor;
+	
+	@Column(name="book_image")
+	private Blob bookImage;
+	
+	@Column(name="is_available")
+	private Boolean isAvailable;
+
+}
---
+++ src/main/java/id/co/team8/librarymanagement/model/User.java
@@ -0,0 +1,33 @@
+package id.co.team8.librarymanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="user")
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="user_id")
+	private Integer userId;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="role")
+	private String role;
+	
+	@Column(name="enabled")
+	private Boolean enabled;
+}
